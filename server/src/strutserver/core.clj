9c50f8d85858756c454b8ddbd1debd864bcecbb8
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2012-06-12 01:10:03 -0400

    Added code related to zip bundling
    -Added code to extract resource URLs
    -Added code to download resources at the extracted URLs

--- server/src/strutserver/core.clj
+++ server/src/strutserver/core.clj
@@ -2,7 +2,47 @@
   (:gen-class :extends javax.servlet.http.HttpServlet)
   (:use compojure.core
         ring.util.servlet)
-  (:require [compojure.route :as route]))
+  (:require [compojure.route :as route])
+  (:require [net.cgrand.enlive-html :as html])
+  (:import com.google.appengine.api.urlfetch.URLFetchServiceFactory))
+
+
+(defn extract-resource-urls
+	"Takes the source of a page and extracts urls 
+	to its external resources"
+  [page-src]
+  (let [document (html/html-resource (java.io.StringReader. page-src))]
+    (concat 
+      (map #(html/attr-values %1 :src)
+        (html/select document
+          #{[:img] [:video] [:script]}
+      ))
+      (map #(html/attr-values %1 :href)
+        (html/select document
+          #{[:link]}
+      ))
+      (map #(html/attr-values %1 :src)
+        (html/select document
+          #{[:video :source]}
+      ))
+    )
+  )
+)
+
+(defn resource-downloaded [http-response]
+  (println http-response)
+)
+
+(defn fetch-resources
+  [resource-urls]
+  (let [fetch-service (URLFetchServiceFactory/getURLFetchService)]
+    (doseq [future (map #(.fetchAsync fetch-service %1)
+                    (map #(java.net.URL. %1) resource-urls))]
+      (resource-downloaded (.get future))
+    )
+  )
+)
+
 
 (defroutes example
   (GET "/" [] "<h1>Hello World Wide Web!</h1>")
--- server/test/strutserver/test/core.clj
+++ server/test/strutserver/test/core.clj
@@ -1,6 +1,11 @@
-(ns server.test.core
-  (:use [server.core])
+(ns strutserver.test.core
+  (:use [strutserver.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+
+
+(deftest test-extract-resource-urls
+	(fetch-resources (flatten (map seq 
+		(extract-resource-urls "<html><img src='http://google.com'></img> <script src='http://www.gamers.com'></script> <link href='http://www.about.com'></link></html>"))
+	))
+) ; (is false "No tests have been written.")
