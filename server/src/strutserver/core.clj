3cdae320e7d189345be23e24c1a79dd8e724c2f4
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2012-06-14 01:19:12 -0400

    zipper seems to be fully functional...  Still needs some testing.

--- server/src/strutserver/core.clj
+++ server/src/strutserver/core.clj
@@ -5,7 +5,10 @@
   (:require [compojure.route :as route])
   (:require [net.cgrand.enlive-html :as html])
   (:require [ring.util.response :as response])
-  (:import com.google.appengine.api.urlfetch.URLFetchServiceFactory))
+  (:import com.google.appengine.api.urlfetch.URLFetchServiceFactory)
+  (:import com.google.appengine.api.urlfetch.FetchOptions$Builder)
+  (:import com.google.appengine.api.urlfetch.HTTPMethod)
+  (:import com.google.appengine.api.urlfetch.HTTPRequest))
 
 
 (defn extract-resource-urls
@@ -30,34 +33,49 @@
   )
 )
 
+(defn create-http-request [url]
+  (HTTPRequest. url HTTPMethod/GET (FetchOptions$Builder/followRedirects))
+)
+
 (defn fetch-resources
   [resource-urls]
   (let [fetch-service (URLFetchServiceFactory/getURLFetchService)]
-    (map #(.get %1) 
-      (doall (map #(.fetchAsync fetch-service %1) resource-urls)
-    ))
+    (doall (map #(.get %1)  ; make sure the gets are all run
+      (doall (map #(.fetchAsync fetch-service (create-http-request %1)) resource-urls) ; make sure the fetches are run
+    )))
   )
 )
 
-(defn create-zip [http-responses]
+(defn create-local-url [^java.net.URL url]
+  (str (.hashCode url) (.replace (.replace (.getFile url) "/" ".") "?" "."))
+)
+
+(defn create-zip [http-responses new-markup original-urls]
   (let [buf-out (java.io.ByteArrayOutputStream.)]
   (let [zip-out (java.util.zip.ZipOutputStream. buf-out)]
     (do 
-      (doseq [response http-responses]
+      (doseq [group (map (fn [response origUrl] {:response response :originalUrl origUrl}) http-responses original-urls)]
         (do 
-          (.putNextEntry zip-out (java.util.zip.ZipEntry. (str (.hashCode (.getFinalUrl response)))))
-          (.write zip-out (.getContent response))
+          (println (.getResponseCode (:response group)) (.getFinalUrl (:response group)))
+          (.putNextEntry zip-out (java.util.zip.ZipEntry. (create-local-url (:originalUrl group))))
+          (.write zip-out (.getContent (:response group)))
           (.closeEntry zip-out)
         ))
+      (.putNextEntry zip-out (java.util.zip.ZipEntry. "index.html"))
+      (.write zip-out (.getBytes new-markup))
+      (.closeEntry zip-out)
       (.close zip-out)
       (java.io.ByteArrayInputStream. (.toByteArray buf-out))
     )
   ))
 )
 
-(defn replace-urls [processed-urls]
+(defn replace-urls [processed-urls markup]
   ; todo: replace the urls in the document
-  (vals processed-urls)
+  ; find [src=(keys processed-urls)] and replace with
+  ; (create-local-url (keys processed-urls))
+  {:urls (vals processed-urls) :markup 
+    (reduce #(clojure.string/replace %1 (first %2) (create-local-url (last %2))) markup processed-urls)}
 )
 
 (defn preprocess-urls [resource-urls]
@@ -67,12 +85,11 @@
 )
 
 (defn build-zip-response [markup]
-  (let [zip-file (-> markup
-        extract-resource-urls
-        preprocess-urls
-        replace-urls
+  (let [urls-and-markup 
+    (replace-urls (preprocess-urls (extract-resource-urls markup)) markup)]
+    (let [zip-file (-> (:urls urls-and-markup)
         fetch-resources
-        create-zip
+        (create-zip (:markup urls-and-markup) (:urls urls-and-markup))
       )]
     (->
       (response/response zip-file)
@@ -82,12 +99,13 @@
       (response/header "Content-Transfer-Encoding" "binary")
       (response/header "Content-Description" "File Transfer")
     ))
+  )
 )
 
 (defroutes example
   (GET "/" [] "I'm a babb")
   (GET "/debug/zip" []
-    (build-zip-response "<img src='http://google.com'></img><link href='http://yahoo.com'></link><script src='http://cnn.com'></script>")
+    (build-zip-response "<img src='http://clojuredocs.org/images/cd_logo.png'></img><link href='http://html5boilerplate.com/css/style.css?v3launch'></link><script src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>")
   )
   (GET "/zip/:markup" [markup]
     (build-zip-response markup)
