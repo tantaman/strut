c65a066fade6efe922b81352422e0d5f5d1da7b0
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2012-07-02 16:44:06 -0400

    Added a target to build and concatenate the vendor scripts. Added jsZip. Removed shader loader that found its way into the project.

--- client/web/scripts/vendor/amd_plugins/shader.js
+++
@@ -1,9 +0,0 @@
-define(function() {
-	return {
-		//node.attachEvent("onreadystatechange", callback);
-	    load: function (name, req, load, config) {
-	    	var shaderTag = document.createElement("script");
-	    	shaderTag.type = "x-shader/x-fragment";
-	    }
-	}
-});
