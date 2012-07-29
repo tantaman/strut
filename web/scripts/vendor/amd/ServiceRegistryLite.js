396d69f14a5a8e3b8487259e3e50c47e3c96bf3d
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2012-07-29 14:27:06 -0400

    Documented the service registry lite

--- web/scripts/vendor/amd/ServiceRegistryLite.js
+++ web/scripts/vendor/amd/ServiceRegistryLite.js
@@ -1,9 +1,26 @@
+/**
+@author Matt Crinklaw-Vogt
+*/
 define(function() {
+	/**
+	* Allows on to register objects or functions under interface names
+	* and later retrieve them under those same interface names.
+	*
+	* Multiple services may be registered under the same interface name.
+	* A single service may be also registered under multiple interface names.
+	* @class ServiceRegsitry
+	*/
 	function ServiceRegistry() {
 		this._services = {};
 	}
 
 	ServiceRegistry.prototype = {
+		/**
+		Register the service under the given interface name(s)
+		@method register
+		@param {Array|String} interfaces string or array of strings of the interface names
+		@param {Object|function} service service to be registered
+		*/
 		register: function(interfaces, service) {
 			if (typeof interfaces === 'string') {
 				interfaces = [interfaces];
@@ -20,6 +37,13 @@ define(function() {
 			}, this);
 		},
 
+		/**
+		Get the first service that has been registered under the given interface
+		name(s)
+		@method get
+		@param {Array|String} interface or interfaces that the service should implement
+		@return {Object|Function} the registered service or null
+		*/
 		get: function(interfaces) {
 			var services = this.getAll(interfaces);
 			if (services != null) {
@@ -27,6 +51,11 @@ define(function() {
 			}
 		},
 
+		/**
+		Get all services registered under the given interface(s)
+		@method getAll
+		@param {Array|String} interfaces
+		*/
 		getAll: function(interfaces) {
 			if (typeof interfaces === 'string') {
 				return this._services[interfaces];
