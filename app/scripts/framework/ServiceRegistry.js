b713014ed7d48ab8027ee4631c7381914e4794fe
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2012-12-09 18:10:14 -0500

    Added unit tests and basic service registry impl

--- app/scripts/framework/ServiceRegistry.js
+++ app/scripts/framework/ServiceRegistry.js
@@ -1,17 +1,36 @@
-define(function() {
+define(['common/EventEmitter', 'common/collections/MultiMap'],
+function(EventEmitter, MultiMap) {
 	function ServiceRegistry() {
-		this._services = {};
+		this._services = new MultiMap();
 	}
 
-	ServiceRegistry.prototype = {
+	var proto = ServiceRegistry.prototype = Object.create(EventEmitter.prototype);
 
+	proto.register = function(opts, service) {
+		var interfaces = Array.isArray(opts.interfaces) ? opts.interfaces : [opts.interfaces];
+
+		var entry = new ServiceEntry(interfaces, opts.meta, service);
+
+		interfaces.forEach(function(iface) {
+			this._services.put(iface, entry);
+		}, this);
 	};
 
-	function ServiceEntry() {
-
+	function ServiceEntry(interfaces, meta, service) {
+		this._interfaces = interfaces;
+		this._meta = meta;
+		this._service = service;
 	}
 
 	ServiceEntry.prototype = {
+		equals: function(other) {
 
+		},
+
+		matches: function(other) {
+			
+		}
 	};
+
+	return ServiceRegistry;
 });
---
+++ test/spec/ServiceRegistryTest.js
@@ -0,0 +1,9 @@
+define([
+	'../../app/scripts/framework/ServiceRegistry'
+], function(ServiceRegistry) {
+	'use strict';
+
+	describe('ServiceRegistry', function() {
+		var registry = new ServiceRegistry();
+	});
+})
