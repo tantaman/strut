30a4c0332a82ad14d9662bc8b8de1d5a59ec716b
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2012-12-22 20:27:23 -0500

    Bringing over components that aren't being totally re-written

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -39,6 +39,11 @@ require.config({
   }
 });
 
+var __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+window.__extends = __extends;
+
 require(['bundles/strut_config/config',
          'features',
          'bundles/editor/view/EditorView',
