54e1d60ed7881f93e16a61737a82c2382c3b387d
Jan-Christoph Borchardt <hey@jancborchardt.net>
2013-04-13 01:04:57 +0300

    add Spanish translation

---
+++ app/locales/es.js
@@ -0,0 +1,13 @@
+define({
+  insert_image: 'Insertar imagen',
+	insert_video: 'Insertar vídeo',
+	insert_website: 'Insertar sitio web',
+	add_slide: 'Añadir diapositiva',
+	remove_slide: 'Eliminar diapositiva',
+	slides: 'Diapositivas',
+	overview: 'Resumen',
+	open: 'Abrir',
+	save: 'Guardar',
+	save_as: 'Guardar como...',
+	new_: 'Nuevo'
+};)
