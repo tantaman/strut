6398a6806c985e92be575d9ff390ff0afd131868
Jan-Christoph Borchardt <hey@jancborchardt.net>
2013-04-13 00:54:50 +0300

    add German translation

---
+++ app/locales/de.js
@@ -0,0 +1,13 @@
+define({
+  insert_image: 'Bild einfügen',
+	insert_video: 'Video einfügen',
+	insert_website: 'Webseite einfügen',
+	add_slide: 'Folie hinzufügen',
+	remove_slide: 'Folie entfernen',
+	slides: 'Folien',
+	overview: 'Übersicht',
+	open: 'Öffnen',
+	save: 'Speichern',
+	save_as: 'Speichern als...',
+	new_: 'Neu'
+});
