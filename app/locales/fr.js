dd4b14586cfdf06790bfb2586c367c723024af86
Sylvain Collilieux <Sylvain@Collilieux.net>
2013-04-13 11:26:27 +0200

    Add french translation

---
+++ app/locales/fr.js
@@ -0,0 +1,18 @@
+define({
+	insert_image: 'Insérer une image',
+	insert_video: 'Insérer une vidéo',
+	insert_website: 'Insérer un site web',
+	add_slide: 'Ajouter une diapo',
+	remove_slide: 'Supprimer la diapo',
+	slides: 'Diapos',
+	overview: 'Vue d\'ensemble',
+	open: 'Ouvrir',
+	save: 'Enregistrer',
+	save_as: 'Enregistrer sous...',
+	new_: 'Nouveau',
+	text: 'Texte',
+	image: 'Image',
+	video: 'Vidéo',
+	website: 'Site wen',
+	present: 'Courant'
+});
