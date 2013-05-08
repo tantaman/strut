00641f9d9adfeef65f5e069fb66dc1bfe25aa876
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-05-08 08:28:27 -0400

    Changed slide backgrounds to solid colors.

--- app/bundles/app/strut.themes/AvailableBackgrounds.js
+++ app/bundles/app/strut.themes/AvailableBackgrounds.js
@@ -2,13 +2,16 @@ define(['css!styles/strut.themes/backgroundClasses.css'], function() {
 	return {
 	backgrounds: [
 	// TODO: translate descriptions
-		{klass: 'bindingDark', description: 'Binding Dark'},
-		{klass: 'blackLozenge', description: 'Black Lozenge'},
-		{klass: 'debutDark', description: 'Debut Dark'},
-		{klass: 'escheresque', description: 'Escheresque'},
-		{klass: 'greyWashWall', description: 'Grey Wash Wall'},
-		{klass: 'mochaGrunge', description: 'Mocha Grunge'},
-		{klass: 'nastyFabric', description: 'Nasty Fabric'},
-		{klass: 'wildOliva', description: 'Wild Oliva'}
+		{klass: 'solid-bg-black', description: 'Black'},
+		{klass: 'solid-bg-light', description: 'Light'},
+		{klass: 'solid-bg-smoke', description: 'Smoke'},
+		{klass: 'solid-bg-orange', description: 'Orange'},
+		{klass: 'solid-bg-yellow', description: 'Yellow'},
+		{klass: 'solid-bg-grass', description: 'Grass'},
+		{klass: 'solid-bg-darkgreen', description: 'Dark Green'},
+		{klass: 'solid-bg-sky', description: 'Sky'},
+		{klass: 'solid-bg-lavender', description: 'Lavender'},
+		{klass: 'solid-bg-purple', description: 'Purple'},
+		{klass: 'solid-bg-salmon', description: 'Salmon'},
 	]
 }});
