98d50cd1caa28ab9ae43d014d7332169c178a9c3
Joachim <>
2013-05-31 16:27:06 +0200

    rpi load injection

--- app/scripts/common/Rpi.js
+++ app/scripts/common/Rpi.js
@@ -1,7 +1,21 @@
-//console.log('RPI LOADED');
+console.log('RPI LOADED');
 
 define(function() {
 
+	function getParam(variable){  
+		var query = window.location.search.substring(1);  
+		var vars = query.split("&");  
+		for (var i=0;i<vars.length;i++) {  
+				var pair = vars[i].split("=");  
+				if(pair[0] == variable){return pair[1];}  
+		}
+		return(false);
+	}
+
+
+	console.log("Lade Ausstellung: "+getParam('p')+" via Ajax");
+	
+	
 
 	function initializeRaphaelIcons() {
 		/**
