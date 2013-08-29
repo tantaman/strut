8cd65c77d468a42ddff280064b89bf896ac80dfa
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-08-29 12:09:56 -0400

    Added shapes to the menu

--- app/bundles/app/strut.slide_components/ShapeCollection.js
+++ app/bundles/app/strut.slide_components/ShapeCollection.js
@@ -1,13 +1,30 @@
 define([],
 function() {
-	function PlatonicShape(name, klass) {
+	function PlatonicShape(name, src, aspect) {
+		if (!(this instanceof PlatonicShape))
+			return new PlatonicShape(name, src, aspect);
+
 		this.name = name;
-		this.klass = klass;
+		this.src = 'preview_export/shapes/' + src;
+		this.aspect = aspect;
 	}
 
 	function ShapeCollection() {
 		this.title = 'shapes';
-		this.shapes = [];
+		this.shapes = [
+			PlatonicShape('square', 'square.svg', 1),
+			PlatonicShape('triangle', 'triangle.svg', 1),
+			PlatonicShape('circle', 'circle.svg', 1),
+			PlatonicShape('hexagon', 'hexagon.svg', 1),
+			PlatonicShape('pentagon', 'pentagon.svg', 1),
+			PlatonicShape('star', 'star.svg', 1),
+			PlatonicShape('pacman', 'pacman.svg', 1),
+			PlatonicShape('heart', 'heart.svg', 1),
+			PlatonicShape('ribbon', 'ribbon.svg', 1),
+			PlatonicShape('infinity', 'infinity.svg', 1),
+			PlatonicShape('yin yang', 'yinyang.svg', 1),
+			PlatonicShape('glasses', 'glasses.svg', 1)
+		];
 	}
 
 	ShapeCollection.prototype = {
