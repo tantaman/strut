db340330b240754d67a5f5c6bb6b7fb33e7d5fc7
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-10-01 10:57:31 -0400

    Wired everything together for the bespoke overview

--- app/bundles/app/strut.transition_editor/view/CannedTransitionsView.js
+++ app/bundles/app/strut.transition_editor/view/CannedTransitionsView.js
@@ -1,5 +1,30 @@
 define(['libs/backbone'], function(Backbone) {
+	var canned = [
+		{
+			name: 'carousel'
+		},
+		{
+			name: 'classic'
+		},
+		{
+			name: 'concave'
+		},
+		{
+			name: 'coverflow'
+		},
+		{
+			name: 'cube'
+		},
+		{
+			name: 'none'
+		}
+	];
+
 	return Backbone.View.extend({
+		events: {
+			'click .thumbnail': '_transitionSelected'
+		},
+
 		className: 'cannedTransitionsView',
 		initialize: function() {
 			// TODO: get the available "canned" transitions
@@ -9,11 +34,28 @@ define(['libs/backbone'], function(Backbone) {
 
 			// Do a survey of the number of presentations
 			// that actually use the "basement" feature...
+			var transition = this.model.cannedTransition() || 'none';
+
+			canned.some(function(c) {
+				if (c.name == transition) {
+					c.active = true;
+				} else {
+					c.active = false;
+				}
+			});
+		},
+
+		_transitionSelected: function(e) {
+			this.$el.find('.active').removeClass('active');
+			var activated = e.currentTarget.dataset.name;
+			this.model.cannedTransition(activated);
+
+			$(e.currentTarget).addClass('active');
 		},
 
 		render: function() {
 			this.$el.html(
-				JST['strut.transition_editor/CannedTransitions']());
+				JST['strut.transition_editor/CannedTransitions'](canned));
 			return this;
 		},
 
@@ -22,7 +64,7 @@ define(['libs/backbone'], function(Backbone) {
 		},
 
 		constructor: function CannedTransitionsView() {
-			Backbone.View.prototype.constructor.call(this);
+			Backbone.View.prototype.constructor.apply(this, arguments);
 		}
 	});
 });
