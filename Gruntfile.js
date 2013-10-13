6665f271444ac1f86b544b2d6c2e06dcde045a2e
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-10-12 23:37:16 -0400

    Added rjs analyze task to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -163,6 +163,17 @@ module.exports = function (grunt) {
                     preserveLicenseComments: false,
                     useStrict: true,
                     wrap: true,
+                    done: function(done, output) {
+                        var duplicates = require('rjs-build-analysis').duplicates(output);
+
+                        if (duplicates.length > 0) {
+                          grunt.log.subhead('Duplicates found in requirejs build:');
+                          grunt.log.warn(duplicates);
+                          done(new Error('r.js built duplicate modules, please check the excludes option.'));
+                        }
+
+                        done();
+                    }
                     //uglify2: {} // https://github.com/mishoo/UglifyJS2
                 }
             }
