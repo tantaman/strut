d2b9cf87e0939cdbc5cf5c0447f6f5498c365855
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-10-17 01:57:58 -0400

    Keeps lls/dist

---
+++ app/components/lls/dist/contrib/S3Link.js
@@ -0,0 +1,13 @@
+LargeLocalStorage.contrib.S3Link = (function() {
+	function S3Link(config) {
+
+	}
+
+	S3Link.prototype = {
+		push: function(docKey, options) {
+
+		}
+	};
+
+	return S3Link;
+})();
