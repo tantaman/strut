2f17139d6227ed4c09bbd6970d43abaf318a68c4
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-12-02 22:49:57 -0500

    Added first core components and notes on model-init

---
+++ app/elements/list-view/list-view.js
@@ -0,0 +1,10 @@
+(function() {
+	// Expect bacon, jquery, etc.?
+
+	/*
+	-Needs a data model which is really just an array
+	-Needs an item renderer to render the data from our model
+		-This will come in via a ctor param or ctor propert
+			-data="^model-init;.modelProp"
+	*/
+})();
