31173e7d57ffbe7106334a2519e0e010f8675683
maninder123 <manindersingh221@gmail.com>
2015-11-13 17:05:45 +0530

    Updated User Ui

--- app/preview_export/scripts/extendedReveal.js
+++ app/preview_export/scripts/extendedReveal.js
@@ -1,41 +1,42 @@
 (function () {
     var autoSLideStatus = 0;
-    var bottonList = "<li style='z-index: 100'><i class='fa fa-bars'></i></li>"
-            + "<li id='autoplay'><i class='fa fa-play'></i></li>"
-            //+ "<li id='refresh'><i class='fa fa-refresh'></i></li>"
-            + "<li id='fullscreen'><i class='fa fa-arrows-alt'></i></li>"
-            + "<li id='allSlide'><h3><i class='fa fa-map-o'></i></li>"
-            + "<li id='download'><i class='fa fa-download'></i></li>";
+    var bottonList = "<li id='autoplay'><img alt='Play' src='Preview-Icons/play.png'></li>"
+            + "<li id='gallery'><img src='Preview-Icons/chart-gallery.png' alt='Gallery'></li>"
+            + "<li id='edit'><img src='Preview-Icons/edit.png' alt='Edit'></li>"
+            + "<li id='refresh'><img src='Preview-Icons/refresh.png' alt='Refresh'></li>"
+            + "<li id='download'><img src='Preview-Icons/download.png' alt='Download'></li>"
+            + "<li id='fullscreen'><img src='Preview-Icons/fullscreen.png' alt='Fullscreen'></li>";
 
 
-    $("body").append("<div style='position:absolute; z-index:1; top:0; right:0;width:30px' id='botton-container'><ul style = 'list-style: none'>" + bottonList + "</ul></div>");
+    $("body").append("<div style='position:absolute; z-index:1; bottom:70px; transform:translate(-270px, 0px); left:50%; border-radius: 10px; background-color: #959699' id='botton-container'><ul style = 'list-style: none'>" + bottonList + "</ul></div>");
 
     $("#botton-container li")
             .css({
-                "position": "absolute",
-                "width": "30px",
-                "height": "30px",
-                "font-size": "15px",
-                "background-color": "#00BAF2",
+//                "position": "absolute",
+                "display": "inline-block",
+                "width": "50px",
+                "height": "50px",
+                "padding": "10px 20px",
+//                "background-color": "#959699",
                 "text-align": "center",
                 "cursor": "pointer",
                 "line-height": "30px",
             });
 
-    $("#botton-container").hover(
-            function () {
-                $("#botton-container li")
-                        .css({position: "static"})
-                        .hover(function () {
-                            $(this).css({"background-color": "#b0e5f4"});
-                        }, function () {
-                            $(this).css({"background-color": "#00BAF2"});
-                        });
-            },
-            function () {
-                $("#botton-container li")
-                        .css({position: "absolute"});
-            });
+//    $("#botton-container").hover(
+//            function () {
+//                $("#botton-container li")
+//                        .css({position: "static"})
+//                        .hover(function () {
+//                            $(this).css({"background-color": "#b0e5f4"});
+//                        }, function () {
+//                            $(this).css({"background-color": "#00BAF2"});
+//                        });
+//            },
+//            function () {
+//                $("#botton-container li")
+//                        .css({position: "absolute"});
+//            });
 
     $("#autoplay").click(function () {
         Reveal.configure({autoSlide: (autoSLideStatus = autoSLideStatus ? 0 : 2000)});
@@ -47,9 +48,10 @@
 
     $("#fullscreen").click(function () {
         Reveal.fullScreen();
+       
     });
 
-    $("#allSlide").click(function () {
+    $("#gallery").click(function () {
         Reveal.toggleOverview();
     });
 
@@ -94,3 +96,35 @@
     $(".slide-number").css({"text-align": "center"});
 
 }());
+
+var parseQueryString = function() {
+
+    var str = window.location.search;
+    var objURL = {};
+
+    str.replace(
+        new RegExp( "([^?=&]+)(=([^&]*))?", "g" ),
+        function( $0, $1, $2, $3 ){
+            objURL[ $1 ] = $3;
+        }
+    );
+    return objURL;
+};
+
+var params = parseQueryString();
+
+if(typeof params.id !== "undefined" && params.id !== "") {
+$.ajax({
+  method: "POST",
+  url: (params.id + ".json")
+})
+  .success(function( dataStr ) {
+      var slideData = jQuery.parseJSON(dataStr);
+      $("iframe").each(function(i, el){
+          $(el).attr("src", slideData.slides[i].components[0].src);
+          
+      });
+      
+  });
+  
+  }
