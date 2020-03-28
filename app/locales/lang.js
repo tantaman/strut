d8ae4ffac88f91b1084dc7d31032ebb141e04f06
albanobattistella <34811668+albanobattistella@users.noreply.github.com>
2020-03-28 12:30:57 +0100

    Update lang.js

--- app/locales/lang.js
+++ app/locales/lang.js
@@ -2,16 +2,18 @@ define(['../locales/de',
 		'../locales/en',
 		'../locales/es',
 		'../locales/fr',
+	        '../locales/it',
 		'../locales/nl',
 		'../locales/ru',
 		'../locales/ar',
 		'handlebars'],
-function(de, en, es, fr, nl, ru, ar, handlebars) {
+function(de, en, es, fr, it, nl, ru, ar, handlebars) {
 	var langs = {
 		en: en,
 		de: de,
 		es: es,
 		fr: fr,
+		it:  it,
 		nl: nl,
 		ru: ru,
 		ar: ar
