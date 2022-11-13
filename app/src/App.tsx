475fbde8f74320339d72b823f61d429b71269774
Matt <1009003+tantaman@users.noreply.github.com>
2022-11-13 01:44:23 -0500

    start bootstrapping and seeding

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { AppState } from "./domain/schema";
 import { Ctx } from "./hooks";
 
 export default function App({ ctx }: { ctx: Ctx }) {
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -8,7 +8,8 @@ import { Ctx } from "./hooks.js";
 import sqliteWasm from "@vlcn.io/wa-crsqlite";
 import tblrx from "@vlcn.io/rx-tbl";
 import wdbRtc from "@vlcn.io/network-webrtc";
-import { tableNames, tables } from "./domain/schema.js";
+import { AppState, tableNames, tables } from "./domain/schema.js";
+import mutations from "./domain/mutations.js";
 
 async function main() {
   const sqlite = await sqliteWasm();
@@ -16,7 +17,7 @@ async function main() {
   const db = await sqlite.open("strut3");
   (window as any).db = db;
 
-  // await db.execMany(tableNames.map((n) => `DROP TABLE IF EXISTS ${n};`));
+  await db.execMany(tableNames.map((n) => `DROP TABLE IF EXISTS ${n};`));
 
   await db.execMany(tables);
   const r = await db.execA<[Uint8Array]>("SELECT crsql_siteid()");
@@ -40,6 +41,18 @@ async function main() {
 function startApp(ctx: Ctx) {
   (window as any).ctx = ctx;
   const root = createRoot(document.getElementById("content")!);
+
+  // const appState: AppState = {
+  //   ctx,
+  //   editor_mode: "slide",
+  //   current_deck_id: await mutations.getOrCreateCurrentDeck(ctx),
+  //   open_type: false,
+  //   drawing: false,
+  //   authoringState: {},
+  //   previewTheme: {},
+  //   deckIndex: {},
+  // };
+
   root.render(<App ctx={ctx} />);
 }
 
