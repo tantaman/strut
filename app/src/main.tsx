9f172c5f19319ef8167df63e9c32f6f080db5d76
Matt (tantaman) <1009003+tantaman@users.noreply.github.com>
2022-12-22 15:00:50 -0500

    update vlcn deps Summary:
    Test Plan:

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -25,7 +25,7 @@ import startSyncWith from "@vlcn.io/sync-client";
 async function main() {
   const sqlite = await sqliteWasm((file) => wasmUrl);
 
-  const db = await sqlite.open("strut7");
+  const db = await sqlite.open("strut9");
   (window as any).db = db;
 
   // TODO: upgrade to common dev env reset fn
