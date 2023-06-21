746e58282bca941fec1de4396e1b1ff15236789c
Matt <1009003+tantaman@users.noreply.github.com>
2023-06-21 12:40:41 -0400

    preview image on insertion

--- src/components/editor/embed/EmbedModal.tsx
+++ src/components/editor/embed/EmbedModal.tsx
@@ -1,3 +1,56 @@
-export default function EmbedModal() {
-  return <div></div>;
+import { useState } from "react";
+import AppState from "../../../domain/ephemeral/AppState";
+import style from "./EmbedModal.module.css";
+import { ChangeEvent } from "react";
+
+export default function EmbedModal({ appState }: { appState: AppState }) {
+  const [url, setUrl] = useState("");
+  const [readyToPreview, setReadyToPreview] = useState(false);
+  function onCancel() {
+    appState.toggleModal("addEmbed", false);
+  }
+
+  function onOk() {
+    // run the mutations to add to the deck...
+    // sanity check url
+    appState.toggleModal("addEmbed", false);
+  }
+
+  function onInputChange(e: ChangeEvent<HTMLInputElement>) {
+    setUrl(e.target.value);
+    try {
+      new URL(e.target.value);
+      setReadyToPreview(true);
+    } catch (e) {
+      setReadyToPreview(false);
+    }
+  }
+
+  return (
+    <div className={style.root}>
+      <h3>Add Image</h3>
+      <hr></hr>
+      <div style={{ marginBottom: 12 }}>
+        <label>URL: </label>
+        <span className="spacer"></span>
+        <input type="text" onChange={onInputChange} value={url}></input>
+      </div>
+      <div className={style.previewImg}>
+        {readyToPreview ? <img src={url} /> : null}
+      </div>
+      <div className="pull-right">
+        <button
+          onClick={onCancel}
+          type="button"
+          className="btn btn-secondary"
+          style={{ marginRight: 10 }}
+        >
+          Cancel
+        </button>
+        <button type="button" className="btn btn-dark" onClick={onOk}>
+          Ok
+        </button>
+      </div>
+    </div>
+  );
 }
